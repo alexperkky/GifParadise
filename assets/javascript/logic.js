4fdbd1903f5bea55eb6c0ac633be2aa225e85cad
Alexperkky <alex.perkinson5@gmail.com>
2018-09-29 22:56:59 -0700

    Mostly done, a few tweaks to make

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,53 @@
-// alert("test");
+$(function(){
+    populateButtons(searchArray, 'searchButton','#buttonsArea');
+})
+
+var searchArray = ["The Rock", "Bret Hart", "Macho Man Randy Savage"];
+
+function populateButtons(searchArray, classToAdd, buttonsArea){
+    $(buttonsArea).empty();
+    for(var i = 0; i < searchArray.length; i++){
+        var a = $('<button>');
+        a.addClass(classToAdd);
+        a.attr('data-type', searchArray[i]);
+        a.text(searchArray[i]);
+        $(buttonsArea).append(a);
+    }
+}
+
+$(document).on('click', '.searchButton', function(){
+    var type = $(this).data('type');
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=kf2xiQqKtS9KhSPKHmOdeFyif8GBKNkY&limit=10';
+    $.ajax({
+        url:queryURL, 
+        method:'GET'})
+    .done(function(response){
+        for(var i = 0; i < response.data.length; i++){
+            var searchDiv = $('<div class ="search-item">');
+            var rating = response.data[i].rating;
+            var p = $('<p>').text('Rating: '+rating);
+            var animated = response.data[i].images.fixed_height.url;
+            var still = response.data[i].images.fixed_height_still.url;
+            var image = $('<img>');
+            image.attr('src', still);
+            image.attr('data-still', still);
+            image.attr('data-animated', animated);
+            image.attr('data-state', 'still');
+            image.addClass('searchImage');
+            searchDiv.append(p);
+            searchDiv.append(image);
+            $('#searches').append(searchDiv);
+        }
+    })
+})
+
+$(document).on('click', 'searchImage', function(){
+    var state = $(this).data('state');
+    if(state == 'still'){
+        $(this).attr('src', $(this).data('animated'));
+        $(this).attr('data-state', 'animated');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+})
